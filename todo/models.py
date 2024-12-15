9b003c918413ee805f7716ffebeced2b29291200
Anisha Rai <anisha6rai@gmail.com>
2024-12-15 21:20:03 +0545

    models view and migrations done

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todolist(models.Model):
+    title = models.CharField(max_length=50)
+    description=models.TextField()
+    state = models.BooleanField(default=False)
--- todo/views.py
+++ todo/views.py
@@ -3,13 +3,19 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def home(request):
     person =[
-        {"name":"Nisha","age":"23"},
-        {"name":"isha","age":"53"},
-        {"name":"miha","age":"13"},
-        {"name":"miha","age":"13"},
-        {"name":"miha","age":"13"},
-        {"name":"miha","age":"13"},
-        {"name":"miha","age":"13"}
+        {"name":"Nisha","age":23},
+        {"name":"isha","age":53},
+        {"name":"miha","age":13},
+        {"name": "Alice", "age": 25},
+        {"name": "Bob", "age": 30},
+        {"name": "Charlie", "age": 22},
+        {"name": "Diana", "age": 28},
+        {"name": "Ethan", "age": 35},
+        {"name": "Fiona", "age": 26},
+        {"name": "George", "age": 29},
+        {"name": "Hannah", "age": 24},
+        {"name": "Ian", "age": 31},
+        {"name": "Julia", "age": 27}
     ]
     context = {
         "key":"Anisha",
